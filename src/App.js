9a981ebe56e0cda1468a44206ab98854c2fb1125
Ana Frizell <acfrizell@gmail.com>
2020-02-12 11:22:30 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 
 export default class App extends Component {
   render(){
-    return <div>Fisher Bookstore</div>;
+    return (
+      <div className="App container">
+        <Navbar bg = "light" expand="lg">
+          <Navbar.Brand>
+            <Link to="/">Fisher Bookstore</Link>
+          </Navbar.Brand>
+        </Navbar>
+        <Routes />
+      </div>
+    );
   }
 }
 
